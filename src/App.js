266f69baf8fa4aaffb26a4a2c077f357c96b29f3
Ankur Bajpai <88947414+ankurbajpai125@users.noreply.github.com>
2023-07-09 23:40:31 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import Todo from "./component/todo";
+
+
+function App() {
+  return (
+    <div className="App">
+      {/* <h1>Hello mutherfuckers, Bajpai is here</h1> */}
+      <Todo/>
+    </div>
+  );
+}
+
+export default App;
